2a74b606499c06563ab2afdcedd832453c1130d5
Jamie Mair <jamie.mair@hotmail.co.uk>
2020-06-22 11:49:27 +0100

    Updated default hyperparameters

--- src/main.py
+++ src/main.py
@@ -7,19 +7,19 @@ from envs.excursion_env import ExcursionEnv
 ray.init()
 config = sac.DEFAULT_CONFIG.copy()
 config["env"] = ExcursionEnv
-config["env_config"] = {"excursion_time": 100, "win_reward": 10.0, "lose_reward": -2.0, "negative_reward": -5.0}
+config["env_config"] = {"excursion_time": 100, "win_reward": 0.0, "lose_reward": -10.0, "negative_reward": -5.0}
 config["eager"] = False
-config["Q_model"]["fcnet_hiddens"] = [16, 16]
-config["policy_model"]["fcnet_hiddens"] = [16, 16]
+config["Q_model"]["fcnet_hiddens"] = [32, 32]
+config["policy_model"]["fcnet_hiddens"] = [32, 32]
 config["initial_alpha"] = 1.0
 config["optimization"]["entropy_learning_rate"] = 0.0
 config["num_gpus"] = 0
-config["num_workers"] = 1
+config["num_workers"] = 16-1
 
 
 tune.run(
     "SAC",
-    stop={"episode_reward_mean": 60},
+    stop={"episode_reward_mean": -1},
     config=config,
 )
 ray.shutdown()
